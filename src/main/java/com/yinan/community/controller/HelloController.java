b6aeffb10768258420a2b7246e524215cf571b5c
Joy <1318434690@qq.com>
2021-08-12 21:46:39 +0800

    first commit

---
+++ src/main/java/com/yinan/community/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.yinan.community.controller;
+
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @auther xingyinan
+ * @create 2021-08-12 21:27
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
